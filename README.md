93bb90cb4146e74f736be73d07eb69e47071ea96
Eduardo Pacheco <eduardopachecoquintanar@gmail.com>
2023-01-11 12:24:27 -0600

    Moved to GitHub

---
+++ README.md
@@ -0,0 +1,5 @@
+# Juegos con X
+
+Lista de juegos disponibles
+
+1. Ahorcado
---
+++ src/components/HangImage.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+  attempts: number;
+}>();
+
+</script>
+
+<template>
+  <img :src="`./src/assets/${props.attempts}.png`" :alt="'localAttprops.attemptsempts'" />
+</template>
+
+<style scoped>
+img {
+  width: 250px;
+}
+</style>

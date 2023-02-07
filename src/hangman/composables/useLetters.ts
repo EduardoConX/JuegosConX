02050d06f0dad79624710d59c5f448b2dff808dd
Eduardo Pacheco <eduardopachecoquintanar@gmail.com>
2023-02-06 20:53:25 -0600

    Cant use again already used letters

---
+++ src/hangman/composables/useLetters.ts
@@ -0,0 +1,56 @@
+import { ref } from "vue";
+import { getRandomWord } from "../helpers/getRandomWord";
+
+const attempts = ref(0);
+const word = ref(getRandomWord());
+const hiddenWord = ref("_ ".repeat(word.value.length));
+const lose = ref(false);
+const won = ref(false);
+const usedLetters = ref<string[]>([]);
+
+const checkLetter = (letter: string) => {
+    usedLetters.value.push(letter);
+    if (lose.value) return;
+    if (won.value) return;
+
+    if (!word.value.includes(letter)) {
+        attempts.value = Math.min(attempts.value + 1, 9);
+        if (attempts.value >= 9) lose.value = true;
+        return;
+    }
+
+    const hiddenWordArray = hiddenWord.value.split(" ");
+
+    for (let i = 0; i < word.value.length; i++) {
+        if (word.value[i] === letter) {
+            hiddenWordArray[i] = letter;
+        }
+    }
+    hiddenWord.value = hiddenWordArray.join(" ");
+    const currentHiddenWord = hiddenWord.value.split(" ").join("");
+
+    if (currentHiddenWord == word.value) won.value = true;
+};
+
+const newGame = () => {
+    word.value = getRandomWord();
+    hiddenWord.value = "_ ".repeat(word.value.length);
+    attempts.value = 0;
+    lose.value = false;
+    won.value = false;
+};
+
+const useLetters = () => {
+    return {
+        word,
+        hiddenWord,
+        attempts,
+        won,
+        lose,
+        usedLetters,
+        checkLetter,
+        newGame
+    }
+}
+
+export default useLetters;
--- src/hangman/pages/HangManPage.vue
+++ src/hangman/pages/HangManPage.vue
@@ -1,57 +1,33 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import { letters } from "../helpers/letters";
-import { getRandomWord } from "../helpers/getRandomWord";
 import HangImage from "../components/HangImage.vue";
+import useLetters from "../composables/useLetters";
 
-const attempts = ref(0);
-const word = ref(getRandomWord());
-const hiddenWord = ref("_ ".repeat(word.value.length));
-const lose = ref(false);
-const won = ref(false);
-
-const checkLetter = (letter: string) => {
-  if (lose.value) return;
-  if (won.value) return;
-
-  if (!word.value.includes(letter)) {
-    attempts.value = Math.min(attempts.value + 1, 9);
-    if (attempts.value >= 9) lose.value = true;
-    return;
-  }
-
-  const hiddenWordArray = hiddenWord.value.split(" ");
-
-  for (let i = 0; i < word.value.length; i++) {
-    if (word.value[i] === letter) {
-      hiddenWordArray[i] = letter;
-    }
-  }
-  hiddenWord.value = hiddenWordArray.join(" ");
-  const currentHiddenWord = hiddenWord.value.split(" ").join("");
-
-  if (currentHiddenWord == word.value) won.value = true;
-};
-
-const newGame = () => {
-  word.value = getRandomWord();
-  hiddenWord.value = "_ ".repeat(word.value.length);
-  attempts.value = 0;
-  lose.value = false;
-  won.value = false;
-};
+const {
+  word,
+  hiddenWord,
+  attempts,
+  won,
+  lose,
+  usedLetters,
+  checkLetter,
+  newGame,
+} = useLetters();
 </script>
 
 <template>
   <div class="text-center text-white text-2xl">
     <HangImage :attempts="attempts" />
     <h2 class="font-bold text-3xl mb-2">{{ hiddenWord }}</h2>
-    <h3 class="font-bold">Intentos: {{ attempts }}</h3>
-    <p v-if="lose">Usted ha perdido, la palabra era {{ word }}</p>
-    <p v-if="won">Usted ha ganado, felicidades</p>
+    <h3>Intentos fallidos: {{ attempts }}</h3>
+    <p class="font-bold" v-if="lose">
+      Usted ha perdido, la palabra era {{ word }}
+    </p>
+    <p class="font-bold" v-if="won">Usted ha ganado, felicidades</p>
     <div class="my-4">
       <button
         v-for="letter in letters"
+        :disabled="usedLetters.includes(letter)"
         :key="letter"
         @click="() => checkLetter(letter)"
         class="m-1.5 btn-black"

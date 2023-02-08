305e38ec17abe1d7f5976c527597df5fcf9a2edf
Eduardo Pacheco <eduardopachecoquintanar@gmail.com>
2023-02-08 15:49:22 -0600

    Home in column in mobile devices

--- src/home/pages/HomePage.vue
+++ src/home/pages/HomePage.vue
@@ -2,18 +2,22 @@
 
 <template>
   <div>
-    <ul class="flex flex-row">
-      <li>
+    <ul class="flex flex-col md:flex-row">
+      <li class="mb-8 md:mr-4">
         <span
-          class="py-2 px-4 rounded-lg bg-blue-600 text-white font-bold mr-4 hover:bg-blue-700"
+          class="py-2 px-4 rounded-lg bg-blue-600 text-white font-bold hover:bg-blue-700"
         >
           <RouterLink to="/blackjack"> 21 Black Jack </RouterLink>
         </span>
+      </li>
+      <li class="mb-8 md:mr-4">
         <span
-          class="py-2 px-4 rounded-lg bg-blue-600 text-white font-bold mr-4 hover:bg-blue-700"
+          class="py-2 px-4 rounded-lg bg-blue-600 text-white font-bold hover:bg-blue-700"
         >
           <RouterLink to="/hangman"> Ahorcado </RouterLink>
         </span>
+      </li>
+      <li>
         <span
           class="py-2 px-4 rounded-lg bg-blue-600 text-white font-bold hover:bg-blue-700"
         >

eaa03f0a2fe6e5b2acd69665fff0b4df888e2e66
Eduardo Pacheco <eduardopachecoquintanar@gmail.com>
2025-03-28 13:29:58 -0600

    Starting fresh

--- README.md
+++
@@ -1,7 +0,0 @@
-# Juegos con X
-
-Lista de juegos disponibles
-
-1. Ahorcado
-2. Adivina el Pokémon
-3. 21 Blackjack
--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { RouterView } from "vue-router";
-</script>
-
-<template>
-  <main class="w-4/5 mx-auto pt-12">
-    <RouterView />
-  </main>
-</template>

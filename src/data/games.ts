a37967879fa9789f5dcc2b54f46a333a11cfaf38
Eduardo Pacheco <eduardopachecoquintanar@gmail.com>
2025-04-03 15:04:50 -0600

    Games imported to HomeView from external file

---
+++ src/data/games.ts
@@ -0,0 +1,22 @@
+export const games = [
+    {
+        title: 'Simon Says',
+        description: "Follow orders and don't make mistakes.",
+        img: 'Simon',
+    },
+    {
+        title: 'Guess the Pokemon',
+        description: "Who's that Pokémon game",
+        img: 'Pokemon',
+    },
+    {
+        title: 'Hangman',
+        description: "Guess the word before it's too late",
+        img: 'HangMan',
+    },
+    {
+        title: 'BlackJack',
+        description: 'The classic card game',
+        img: 'BlackJack',
+    },
+]
--- src/home/views/HomeView.vue
+++ src/home/views/HomeView.vue
@@ -1,28 +1,7 @@
 <script setup lang="ts">
 import SideBar from '@/components/SideBar.vue'
 import FeaturedGame from '../components/FeaturedGame.vue'
-const games = [
-  {
-    title: 'Simon Says',
-    description: "Follow orders and don't make mistakes.",
-    img: 'Simon',
-  },
-  {
-    title: 'Guess the Pokemon',
-    description: "Who's that Pokémon game",
-    img: 'Pokemon',
-  },
-  {
-    title: 'Hangman',
-    description: "Guess the word before it's too late",
-    img: 'HangMan',
-  },
-  {
-    title: 'BlackJack',
-    description: 'The classic card game',
-    img: 'BlackJack',
-  },
-]
+import { games } from '@/data/games';
 </script>
 
 <template>
